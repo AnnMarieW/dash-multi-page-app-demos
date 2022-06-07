e4525e67064e1ccb0316c021ae9c6b2b3c92764e
AnnMarieW <amward@fastmail.com>
2022-06-07 14:52:27 -0700

    added caching examples

---
+++ multi_page_cache/app.py
@@ -0,0 +1,48 @@
+from dash import Dash, html, dcc
+import dash
+
+
+## Diskcache
+import diskcache
+
+cache_lc = diskcache.Cache("./cache_lc")
+
+
+external_stylesheets = [
+    # Dash CSS
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
+    # Loading screen CSS
+    "https://codepen.io/chriddyp/pen/brPBPO.css",
+]
+
+
+app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    [
+        html.H1("Multi-page cache examples"),
+        html.Div(
+            [
+                html.Div(
+                    dcc.Link(
+                        f"{page['name']}", href=page["path"]
+                    )
+                )
+                for page in dash.page_registry.values()
+            ]
+        ),
+        html.Hr(),
+        dcc.Markdown(
+            """
+            For more information, see dash tutorial Sharing Data Between Callbacks chapter
+              - [Example 3 Caching and Signaling](https://dash.plotly.com/sharing-data-between-callbacks#example-3---caching-and-signaling)
+              - [Example 4 User Based Session Data on the server](https://dash.plotly.com/sharing-data-between-callbacks#example-4---user-based-session-data-on-the-server)
+
+            """, style={"margin": 20}
+        ),
+        dash.page_container
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True,  processes=6, threaded=False)
